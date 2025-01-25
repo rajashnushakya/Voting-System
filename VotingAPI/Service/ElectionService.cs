e488f4aa090e719458078b5d301a5a095d21bb23
Rajashnu <rajashnushakya@gmail.com>
2025-01-25 23:19:33 +0545

    added service for the election

---
+++ VotingAPI/Service/ElectionService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using VotingAPI.Model;
+
+namespace VotingAPI.Service
+{
+    public class ElectionService
+    {
+
+        private readonly string _connectionString;
+        public ElectionService(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+        public async Task<DbResponse> AddElectionAsync(Election election, CancellationToken cancellationToken)
+        {
+            DataAccess dataAccess = new DataAccess(_connectionString);
+            SqlCommand cmd = dataAccess.CreateCommand("sp_election_add");
+            //voter details
+            cmd.Parameters.AddWithValue("@ElectionName", election.Name);
+            cmd.Parameters.AddWithValue("@StartDate", election.Start_date);
+            cmd.Parameters.AddWithValue("@EndDate", election.Name);
+            return await dataAccess.ExecuteNonQueryAsync(cancellationToken);
+        }
+    }
+}
