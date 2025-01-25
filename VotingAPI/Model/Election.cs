4b86b4984ddadf954f3f7f0fed7e62a225a72eb6
Rajashnu <rajashnushakya@gmail.com>
2025-01-25 23:17:56 +0545

    Added model for election

---
+++ VotingAPI/Model/Election.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace VotingAPI.Model
+{
+    public class Election
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly Start_date { get; set; }
+        public DateOnly End_date { get; set; }
+    }
+}
