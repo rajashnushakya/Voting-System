81e8c1145b3b7444027c969cfa21417d7762e3cf
Rajashnu <rajashnushakya@gmail.com>
2025-01-25 23:18:48 +0545

    Added service for the Candidate

---
+++ VotingAPI/Service/CandidateService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using VotingAPI.Model;
+
+namespace VotingAPI.Service
+{
+    public class CandidateService
+    {
+        private readonly string _connectionString;
+
+        public CandidateService(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public async Task<DbResponse> AddCandidateAsync(Candidate candidate, CancellationToken cancellationToken)
+        {
+           
+            DataAccess dataAccess = new DataAccess(_connectionString);
+            SqlCommand cmd = dataAccess.CreateCommand("InsertCandidate"); 
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@CandidateId", candidate.CandidateId);
+            cmd.Parameters.AddWithValue("@FullName", candidate.FullName);
+            cmd.Parameters.AddWithValue("@PartyId", candidate.PartyId);
+            cmd.Parameters.AddWithValue("@FatherName", candidate.FatherName);
+            cmd.Parameters.AddWithValue("@MotherName", candidate.MotherName);
+            cmd.Parameters.AddWithValue("@GrandFatherName", candidate.GrandFatherName);
+            cmd.Parameters.AddWithValue("@GrandMotherName", candidate.GrandMotherName);
+            cmd.Parameters.AddWithValue("@DateOfBirth", candidate.Dateofbirth);
+            cmd.Parameters.AddWithValue("@Gender", candidate.gender);
+            cmd.Parameters.AddWithValue("@District", candidate.District);
+            cmd.Parameters.AddWithValue("@Municipality", candidate.Municipality);
+            return await dataAccess.ExecuteNonQueryAsync(cancellationToken);
+            
+        }
+    }
+}
