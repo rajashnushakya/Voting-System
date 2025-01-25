3be1ddb4a20601bb8c01e2f659018689209abd58
Rajashnu <rajashnushakya@gmail.com>
2025-01-25 23:19:12 +0545

    Added controller for candidate

---
+++ VotingAPI/Controllers/CandidateController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VotingAPI.Model;
+using VotingAPI.Service;
+
+namespace VotingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public CandidateController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("VotingDb");
+        }
+
+        [HttpPost]
+        public async Task<DbResponse> RegisterCandidate(Candidate candidate, CancellationToken cancellationToken)
+        {
+            CandidateService candidateService = new CandidateService(_connectionString);
+            return await candidateService.AddCandidateAsync(candidate, cancellationToken);  
+        }
+    }
+}
