6f230830832c2f96b917b2ff54761486e545d8a3
Rajashnu <rajashnushakya@gmail.com>
2025-01-25 23:17:32 +0545

    Added model for candidate

---
+++ VotingAPI/Model/Candidate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Writers;
+
+namespace VotingAPI.Model
+{
+    public class Candidate
+    {
+        public int CandidateId { get; set; }
+        public string FullName { get; set; }
+
+        public int PartyId { get; set; }
+        public string FatherName { get; set; }
+
+        public string MotherName { get; set; }
+        public string GrandFatherName { get; set; }
+        public string GrandMotherName { get; set; }
+        public DateOnly Dateofbirth { get; set; }
+        public string gender { get; set; }
+        public string  District {  get; set; }
+        public string Municipality { get; set; }
+
+
+
+    }
+}
