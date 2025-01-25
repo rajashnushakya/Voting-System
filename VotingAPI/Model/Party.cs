902e9212b175b56c25e069c11f2c0bb6857cf498
Rajashnu <rajashnushakya@gmail.com>
2025-01-25 23:16:36 +0545

    Added model for party

---
+++ VotingAPI/Model/Party.cs
@@ -0,0 +1,10 @@
+﻿namespace VotingAPI.Model
+{
+    public class Party
+    {
+        public int Id { get; set; } 
+        public string PartyName { get; set; }
+        public string Address { get; set; }
+        public string Logo { get; set; }
+    }
+}
