039219987f05c3e06d13e5b950943b6c56d845fc
Rajashnu <rajashnushakya@gmail.com>
2025-01-25 23:20:00 +0545

    added controller for the election

---
+++ VotingAPI/Controllers/ElectionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VotingAPI.Model;
+using VotingAPI.Service;
+
+namespace VotingAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ElectionController : ControllerBase
+    {
+        private readonly string _connectionString;
+
+        public ElectionController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("VotingDb");
+        }
+
+        [HttpPost]
+        public async Task<DbResponse> AddAsync(Election election, CancellationToken cancellationToken) {
+            ElectionService electionService = new ElectionService(_connectionString);
+            return await electionService.AddElectionAsync(election, cancellationToken);
+            
+        }
+    }
+}
