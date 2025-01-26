1eaafb969e0dc0cf48c3ec3d32cb0fd08f207faa
Rajashnu <rajashnushakya@gmail.com>
2025-01-26 10:39:31 +0545

    app use vuetify

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; 
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+// Create Vuetify instance
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App)
+app.use(vuetify);
 app.use(router);
 app.mount('#app');
