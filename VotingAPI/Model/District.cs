d2ce5112392410f0dfb6126bb0adfd983ea50210
Rajashnu <rajashnushakya@gmail.com>
2025-01-26 10:44:50 +0545

    added model for district

---
+++ VotingAPI/Model/District.cs
@@ -0,0 +1,11 @@
+﻿namespace VotingAPI.Model
+{
+    public class District
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+
+        public List<Municipality> Municipalities { get; set; }
+    }
+
+}
