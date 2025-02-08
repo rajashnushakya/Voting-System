a4c98a2460c1cd66c6f85d459cd2e346136b60a7
Rajashnu <rajashnushakya@gmail.com>
2025-02-08 23:43:13 +0545

    added dialoge activation

--- web/src/components/childcomponents/MenuComponent.vue
+++ web/src/components/childcomponents/MenuComponent.vue
@@ -1,143 +1,101 @@
-<script setup>
-  import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-  import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-  import { ChevronDownIcon } from '@heroicons/vue/20/solid'
-  const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  }
-  const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Election', href: '#', current: false },
-    { name: 'Logs', href: '#', current: false },
-    { name: 'Result', href: '#', current: false },
-    { name: 'Setting', href: '#', current: false },
-  ]
-  const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
-  ]
-
-  </script>
-
 <template>
-    <div class="min-h-full">
-      <Disclosure as="nav" class="bg-nepal-blue" v-slot="{ open }">
-        <div class="mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex h-16 items-center justify-between">
-            <div class="flex items-center">
-              <div class="shrink-0">
-                <img class="size-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-              </div>
-              <div class="hidden md:block">
-                <div class="ml-10 flex items-baseline space-x-4">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-nepal-dark-blue text-white' : 'text-gray-300 hover:bg-nepal-dark-blue hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
-                  <Menu as="div" class="relative inline-block text-left">
-    <div>
-      <MenuButton class="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-nepal-dark-blue">
-        Options
-        <ChevronDownIcon class="-mr-1 size-5 text-white" aria-hidden="true" />
-      </MenuButton>
-    </div>
-
-    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-      <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-        <div class="py-1">
-          <MenuItem v-slot="{ active }">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900 outline-none' : 'text-gray-700', 'block px-4 py-2 text-sm']">Account settings</a>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900 outline-none' : 'text-gray-700', 'block px-4 py-2 text-sm']">Support</a>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a href="#" :class="[active ? 'bg-gray-100 text-gray-900 outline-none' : 'text-gray-700', 'block px-4 py-2 text-sm']">License</a>
-          </MenuItem>
-          <form method="POST" action="#">
-            <MenuItem v-slot="{ active }">
-              <button type="submit" :class="[active ? 'bg-gray-100 text-gray-900 outline-none' : 'text-gray-700', 'block w-full px-4 py-2 text-left text-sm']">Sign out</button>
-            </MenuItem>
-          </form>
-        </div>
-      </MenuItems>
-    </transition>
-  </Menu>
-                </div>
-                
-              </div>
+  <div class="min-h-full">
+    <Disclosure as="nav" class="bg-nepal-blue" v-slot="{ open }">
+      <div class="mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <div class="shrink-0">
+              <img class="size-8" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
             </div>
             <div class="hidden md:block">
-              <div class="ml-4 flex items-center md:ml-6">
-                <button type="button" class="relative rounded-full bg-nepal-blue p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span class="absolute -inset-1.5" />
-                  <span class="sr-only">View notifications</span>
-                  <BellIcon class="size-6" aria-hidden="true" />
-                </button>
-  
-                <!-- Profile dropdown -->
-                <Menu as="div" class="relative ml-3">
-                  <div>
-                    <MenuButton class="relative flex max-w-xs items-center rounded-full bg-nepal-blue text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span class="absolute -inset-1.5" />
-                      <span class="sr-only">Open user menu</span>
-                      <img class="size-8 rounded-full" :src="user.imageUrl" alt="" />
-                    </MenuButton>
-                  </div>
-                  <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                    <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
-                      <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                        <a :href="item.href" :class="[active ? 'bg-nepal-dark-blue outline-none text-white' : '', 'block px-4 py-2 text-sm text-black-100']">{{ item.name }}</a>
-                      </MenuItem>
-                    </MenuItems>
-                  </transition>
-                </Menu>
+              <div class="ml-10 flex items-baseline space-x-4">
+                <template v-for="item in navigation" :key="item.name">
+                  <template v-if="!item.children">
+                    <a 
+                      :href="item.href" 
+                      :class="{
+                        'bg-nepal-dark-blue': item.current, 
+                        'text-white rounded-md px-3 py-2 text-sm font-medium': true, 
+                        'hover:bg-nepal-dark-blue': !item.current
+                      }" 
+                      :aria-current="item.current ? 'page' : undefined" 
+                      @click="setCurrentItem(item)"
+                    >
+                      {{ item.name }}
+                    </a>
+                  </template>
+                  <Menu as="div" v-else class="relative inline-block text-left">
+                    <div>
+                      <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-nepal-dark-blue">
+                        {{ item.name }}
+                        <ChevronDownIcon class="-mr-1 size-5 text-white" aria-hidden="true" />
+                      </MenuButton>
+                    </div>
+                    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                      <MenuItems class="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                        <div class="py-1">
+                          <MenuItem v-for="subItem in item.children" :key="subItem.name" v-slot="{ active }">
+                            <a :href="subItem.href" :class="{
+                              'bg-gray-100 text-gray-900 outline-none': active,
+                              'text-gray-700': !active,
+                              'block px-4 py-2 text-sm': true
+                            }" @click="setCurrentItem(subItem)">
+                              {{ subItem.name }}
+                            </a>
+                          </MenuItem>
+                        </div>
+                      </MenuItems>
+                    </transition>
+                  </Menu>
+                </template>
               </div>
-            </div>
-            <div class="-mr-2 flex md:hidden">
-              <!-- Mobile menu button -->
-              <DisclosureButton class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="absolute -inset-0.5" />
-                <span class="sr-only">Open main menu</span>
-                <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
-                <XMarkIcon v-else class="block size-6" aria-hidden="true" />
-              </DisclosureButton>
             </div>
           </div>
         </div>
-  
-        <DisclosurePanel class="md:hidden">
-          <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
-          </div>
-          <div class="border-t border-gray-700 pb-3 pt-4">
-            <div class="flex items-center px-5">
-              <div class="shrink-0">
-                <img class="size-10 rounded-full" :src="user.imageUrl" alt="" />
-              </div>
-              <div class="ml-3">
-                <div class="text-base/5 font-medium text-white">{{ user.name }}</div>
-                <div class="text-sm font-medium text-gray-400">{{ user.email }}</div>
-              </div>
-              <button type="button" class="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="absolute -inset-1.5" />
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="size-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div class="mt-3 space-y-1 px-2">
-              <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">{{ item.name }}</DisclosureButton>
-            </div>
-          </div>
-        </DisclosurePanel>
-      </Disclosure>
-      <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <!-- Your content -->
-        </div>
-      </main>
-    </div>
-  </template>
-  
-  
+      </div>
+    </Disclosure>
+
+    <AddElection :dialogActive="dialogActive" @update:dialogActive="dialogActive = $event" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import AddElection from './AddElection.vue'; 
+import { Disclosure, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+import { ChevronDownIcon } from '@heroicons/vue/20/solid';
+
+const dialogActive = ref(false);
+
+const navigation = [
+  { name: 'Dashboard', href: 'dashboard', current: false },
+  {
+    name: 'Election',
+    href: '#',
+    current: false,
+    children: [
+      { name: 'Add Election', href: '#add-election' },
+      { name: 'Add Centre', href: '#add-centre' },
+      { name: 'Add Election Centre', href: '#add-election-centre' },
+    ],
+  },
+  { name: 'Result', href: 'result', current: false },
+  { name: 'Setting', href: '#', current: false },
+];
+
+function setCurrentItem(item, isSubItem = false) {
+  navigation.forEach(navItem => {
+    navItem.current = navItem === item;
+
+    if (navItem.children) {
+      navItem.children.forEach(subItem => {
+        subItem.current = subItem === item;
+      });
+    }
+  });
+
+  if (item.name === 'Add Election') {
+    dialogActive.value = true;
+  }
+}
+</script>
