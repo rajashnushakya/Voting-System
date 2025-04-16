5cf0b24f1be6bbfe25058a438741e96676e48d84
Rajashnu <rajashnushakya@gmail.com>
2025-04-16 22:51:49 +0545

    Add CandidateCentre model to represent the association between candidates and election centers

---
+++ VotingAPI/Model/CandidateCentre.cs
@@ -0,0 +1,9 @@
+﻿namespace VotingAPI.Model
+{
+    public class CandidateCentre
+    {
+        public int Id { get; set; }
+        public int CandidateId { get; set; }
+        public int CentreId { get; set; }
+    }
+}
