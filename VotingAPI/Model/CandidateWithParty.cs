4ed8260dfe441b5584e477b348b62cdcc29cde13
Rajashnu <rajashnushakya@gmail.com>
2025-04-17 22:37:47 +0545

    Add CandidateWithParty model: define properties for candidate details and associated party

---
+++ VotingAPI/Model/CandidateWithParty.cs
@@ -0,0 +1,11 @@
+﻿namespace VotingAPI.Model
+{
+    public class CandidateWithParty
+    {
+        public int CandidateId { get; set; }
+        public string FullName { get; set; }
+        public int PartyId { get; set; }
+        public string PartyName { get; set; }
+    }
+
+}
