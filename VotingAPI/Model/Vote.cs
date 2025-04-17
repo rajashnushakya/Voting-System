6972006a88c9c364de8614792434636be8d7b79f
Rajashnu <rajashnushakya@gmail.com>
2025-04-17 22:37:52 +0545

    Add Vote model: define properties for candidate, voter, election, and election centre IDs

---
+++ VotingAPI/Model/Vote.cs
@@ -0,0 +1,10 @@
+﻿namespace VotingAPI.Model
+{
+    public class Vote
+    {
+        public int CandidateId { get; set; }
+        public int VoterId { get; set; }
+        public int ElectionId { get; set; }
+        public int ElectionCentreId { get; set; }
+    }
+}
