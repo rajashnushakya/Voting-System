723de2e101af80c74919df585cabeedb19e58c65
Rajashnu <rajashnushakya@gmail.com>
2025-04-17 22:38:00 +0545

    Add VoteDetails model: define properties for voter, candidate, party, election, and election centre

---
+++ VotingAPI/Model/VoteDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace VotingAPI.Model
+{
+    public class VoteDetails
+    {
+        public int VoterId { get; set; }
+        public int CandidateId { get; set; }
+        public string CandidateName { get; set; }
+        public int PartyId { get; set; }
+        public string PartyName { get; set; }
+        public int ElectionId { get; set; }
+        public string ElectionName { get; set; }
+        public int ElectionCentreId { get; set; }
+        public string ElectionCentreName { get; set; }
+    }
+}
