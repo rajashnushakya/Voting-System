adae197e3391558954944f1178604b8b87a35b42
Rajashnu <rajashnushakya@gmail.com>
2025-04-22 15:18:35 +0545

    add roleid property to AuthResponse for enhanced user role management

--- VotingAPI/Model/User.cs
+++ VotingAPI/Model/User.cs
@@ -24,6 +24,7 @@ namespace VotingAPI.Model
         public string Token { get; set; }
         public DateTime ExpiryDate { get; set; }
         public string voterid { get; set; }
+        public string roleid { get; set; }
     }
 
 }
