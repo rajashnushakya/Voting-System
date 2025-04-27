8654a0b9801aa22c92cd6e47db2db021c698c921
Rajashnu <rajashnushakya@gmail.com>
2025-04-27 13:50:30 +0545

    add: create CandidateVotesByCentre model to represent candidate voting data

---
+++ VotingAPI/Model/CandidateVotesByCentre.cs
@@ -0,0 +1,12 @@
+﻿namespace VotingAPI.Model
+{
+    public class CandidateVotesByCentre
+    {
+        public string CandidateName { get; set; } // Candidate's Full Name
+        public string PartyName { get; set; } // Party Name
+        public int Votes { get; set; } // Total Votes for Candidate
+        public decimal VotePercentage { get; set; } // Vote Percentage (rounded to 2 decimal places)
+    }
+
+
+}
