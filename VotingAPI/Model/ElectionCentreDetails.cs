08b071383cd6fb89b69ee5e591c40a58d3775222
Rajashnu <rajashnushakya@gmail.com>
2025-04-27 12:13:07 +0545

    add ElectionCentreDetails model to represent election center statistics

---
+++ VotingAPI/Model/ElectionCentreDetails.cs
@@ -0,0 +1,12 @@
+﻿namespace VotingAPI.Model
+{
+    public class ElectionCentreDetails
+    {
+
+        public int TotalCandidates { get; set; }
+        public int TotalVoters { get; set; }
+        public int TotalVotes { get; set; }
+    }
+
+
+}
