7558f411d6c18e359b64b5f840916acfb7064db7
Rajashnu <rajashnushakya@gmail.com>
2025-04-27 12:12:51 +0545

    add CentreDetails model to represent election center information

---
+++ VotingAPI/Model/CentreDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace VotingAPI.Model
+{
+    public class CentreDetails
+    {
+        public int CentreId { get; set; }
+        public string CentreName { get; set; }
+        public string DistrictName { get; set; }
+        public string MunicipalityName { get; set; }
+        public int WardNumber { get; set; } 
+    }
+
+
+}
