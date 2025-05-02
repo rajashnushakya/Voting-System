42465a8f2b37254f407a6055e0faad0056282249
Rajashnu <rajashnushakya@gmail.com>
2025-05-03 00:36:11 +0545

    add: create CandidateVoteResult model to represent voting results for candidates

---
+++ VotingAPI/Model/CandidateVoteResult.cs
@@ -0,0 +1,11 @@
+﻿namespace VotingAPI.Model
+{
+    public class CandidateVoteResult
+    {
+        public int CandidateId { get; set; }
+        public string FullName { get; set; }
+        public int VoteCount { get; set; }
+        public string ElectionName { get; set; }
+    }
+
+}
